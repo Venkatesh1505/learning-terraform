c7b8286450db8f14ea90fa9eeaacb95f3009bc89
Venkatesh Ravichandran <venkyravi97@gmail.com>
2023-12-15 16:30:50 +0530

    Create & Attach security group

--- main.tf
+++ main.tf
@@ -17,8 +17,50 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.blog_sg.id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+data "aws_vpc" "blog_vpc" {
+  default = true
+}
+
+resource "aws_security_group" "blog_sg" {
+  name        = "blog_sg"
+  description = "Allow http & https in. Allow everything out."
+
+  vpc_id      = data.aws_vpc.blog_vpc.id
+}
+
+resource "aws_security_group_rule" "http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = [0.0.0.0/0]
+
+  security_group_id = aws_security_group.blog_sg.id
+}
+
+resource "aws_security_group_rule" "https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = [0.0.0.0/0]
+
+  security_group_id = aws_security_group.blog_sg.id
+}
+
+resource "aws_security_group_rule" "everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [0.0.0.0/0]
+
+  security_group_id = aws_security_group.blog_sg.id
 }
