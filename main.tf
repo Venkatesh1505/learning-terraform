ec628eb92791478c430fd3b2f8b0c09dd76ab743
Venkatesh Ravichandran <venkyravi97@gmail.com>
2023-12-15 20:26:37 +0530

    Update security group by module

--- main.tf
+++ main.tf
@@ -17,11 +17,24 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog_sg.id]
+  vpc_security_group_ids = [module.module_security_group.id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "module_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  
+  vpc_id = aws_vpc.blog_vpc.id
+
+  name                = "blog_module_sg"
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 data "aws_vpc" "blog_vpc" {
